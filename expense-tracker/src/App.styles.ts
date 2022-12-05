e3f3898eb946e778806eae6d9d67366f839b5262
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-05 20:11:31 -0300

    styles e arrumado algumas partes do App.tsx

--- expense-tracker/src/App.styles.ts
+++ expense-tracker/src/App.styles.ts
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.div``;
 
+export const Header = styled.div`
+    background-color: darkblue;
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderText = styled.div`
+    font-family: monospace;
+    font-size:50px;
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    padding-top: 30;
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    max-width:980px;
+    margin-bottom:50px;
 `;
--- expense-tracker/src/App.tsx
+++ expense-tracker/src/App.tsx
@@ -3,7 +3,18 @@ import * as C from './App.styles';
 const App = () => {
   return (
     <C.Container>
-      ...
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        
+        {/* Area de informações */}
+
+        {/* Area de inserção */}
+
+        {/* Tabela de itens */}
+
+      </C.Body>
     </C.Container>
   );
 }
