0abbbfa0d4701d6fda6fd0692480253b67846a31
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-05 20:34:46 -0300

    importando

--- expense-tracker/src/App.tsx
+++ expense-tracker/src/App.tsx
@@ -1,13 +1,19 @@
+import {useState} from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
 
 const App = () => {
+  const [list, setList] = useState(items);
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        
+
         {/* Area de informações */}
 
         {/* Area de inserção */}
